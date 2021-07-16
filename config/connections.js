77b9b9d1e11ceb78b655ecbe24e2fdcb3145b629
Andrew <wing_andrew@hotmail.com>
2021-07-15 21:16:45 -0400

    Preparing .env file

--- config/connections.js
+++ config/connections.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: process.env.PORT,
-    user: process.env.DB_USERNAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
   });
     
-  module.exports = connection;
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 //Dependencies
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 const mysql = require('mysql');
-const consoleTable = require("console.table");
+// const consoleTable = require('console.table');
+const connection = require('./config/connections');
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Welcome to the Employee Management System")
+    mainMenu();
+});
+
+function mainMenu (){
+    console.log("Main Menu")
+};
